531179fff4fdf64cd5495b5002aa082a00f0de6f
github username <github email address>
2019-09-18 12:04:26 +0100

    updated shoe class to contain list of brands. updated book class to contain list of genres

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
+  BRANDS = []
+
+  def initialize(shoe_brand)
+    @brand = shoe_brand
+    BRANDS << shoe_brand
+    BRANDS.uniq!
   end
 
   def cobble
